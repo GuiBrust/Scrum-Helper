9db338107702de2b67e5a62f8fe050176e4c7bb1
GuiBrust <53901944+GuiBrust@users.noreply.github.com>
2024-06-11 09:05:21 -0300

    copiar quadro

--- popup.js
+++ popup.js
@@ -1,50 +1,36 @@
-console.log('A extensão está funcionando.');
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    // Agora é seguro usar document aqui
-    document.getElementById('myButton').addEventListener('click', function(){
-        geraQuadro()
-    });
-
-    carregaValoresSalvos()
-
-
-  // Adiciona um listener de eventos para todos os elementos com a classe capturarUrlButton
-  var botoes = document.getElementsByClassName('capturarUrlButton');
-  for (var i = 0; i < botoes.length; i++) {
-    botoes[i].addEventListener('click', capturarUrl);
-  }
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("myButton").addEventListener("click", function () {
+    geraQuadro();
   });
 
-function carregaValoresSalvos(){
+  document.getElementById("clearButton").addEventListener("click", function () {
+    limparCampos();
+  });
 
+  carregaValoresSalvos();
 
-    var textareaIds = [];
+  var botoes = document.getElementsByClassName("capturarUrlButton");
+  for (var i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener("click", capturarUrl);
+  }
+});
 
-  // Adiciona um listener de eventos para todos os elementos com a classe capturarUrlButton
-    var botoes = document.getElementsByClassName('capturarUrlButton');
+function carregaValoresSalvos() {
+  var textareaIds = [];
 
-    for (var i = 0; i < botoes.length; i++) {
-        // Obtém o textarea associado a cada botão
-        var textarea = botoes[i].previousElementSibling;
+  var botoes = document.getElementsByClassName("capturarUrlButton");
 
-        // Adiciona o ID do textarea ao array
-        if (textarea) {
-        textareaIds.push(textarea.id);
-        }
+  for (var i = 0; i < botoes.length; i++) {
+    var textarea = botoes[i].previousElementSibling;
 
-}
+    if (textarea) {
+      textareaIds.push(textarea.id);
+    }
+  }
 
-
-    // Itera sobre os IDs
-textareaIds.forEach(function(textareaId) {
-    // Obtém o valor armazenado no chrome.storage.local para o textarea correspondente ao ID
-    chrome.storage.local.get(textareaId, function(result) {
+  textareaIds.forEach(function (textareaId) {
+    chrome.storage.local.get(textareaId, function (result) {
       if (textareaId in result) {
-        // Atualiza o valor no textarea
         var textarea = document.getElementById(textareaId);
         textarea.value = result[textareaId];
       } else {
@@ -52,51 +38,41 @@ textareaIds.forEach(function(textareaId) {
       }
     });
   });
-
-
 }
 
 function capturarUrl(event) {
-    var botao = event.target;
-    var textarea = botao.previousElementSibling;
+  var botao = event.target;
+  var textarea = botao.previousElementSibling;
 
-   
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentTab = tabs[0];
+    var currentUrl = currentTab.url;
 
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var currentTab = tabs[0];
-        var currentUrl = currentTab.url;
+    var data = {};
+    data[textarea.id] = currentUrl;
 
-        var data = {};
-        data[textarea.id] = currentUrl;
-    
-        textarea.value = currentUrl;
-  
-        chrome.storage.local.set(data).then(() => {
-            //
-          });
+    textarea.value = currentUrl;
 
-      
-      });
+    chrome.storage.local.set(data).then(() => {
+      //
+    });
+  });
+}
+
+function geraQuadro() {
+  var textareaIds = [];
+
+  var botoes = document.getElementsByClassName("capturarUrlButton");
+
+  for (var i = 0; i < botoes.length; i++) {
+    var textarea = botoes[i].previousElementSibling;
+
+    if (textarea) {
+      textareaIds.push(textarea.id);
+    }
   }
 
-
-  function geraQuadro() {
-    var textareaIds = [];
-
-    // Adiciona um listener de eventos para todos os elementos com a classe capturarUrlButton
-    var botoes = document.getElementsByClassName('capturarUrlButton');
-
-    for (var i = 0; i < botoes.length; i++) {
-        // Obtém o textarea associado a cada botão
-        var textarea = botoes[i].previousElementSibling;
-
-        // Adiciona o ID do textarea ao array
-        if (textarea) {
-            textareaIds.push(textarea.id);
-        }
-    }
-
-    var modelo = `h1. Andamento da Sprint
+  var modelo = `h1. Andamento da Sprint
 
 table{width:50%; border: none;}.
 |{border: none;}. "Histórias - Prioridades":%{inputHistorias}|
@@ -107,54 +83,42 @@ table{width:50%; border: none;}.
 |{border: none;}. "Tempo Gasto":%{inputTempoGasto}|
 |{border: none;}. "Relatório de Horas":%{inputRelatorioHoras}|`;
 
-    // Mapeia cada ID para uma Promessa que resolve quando os dados são obtidos do chrome.storage.local
-    var promessas = textareaIds.map(function(textareaId) {
-        return new Promise(function(resolve) {
-            // Obtém o valor armazenado no chrome.storage.local para o textarea correspondente ao ID
-            chrome.storage.local.get(textareaId, function(result) {
-                if (textareaId in result) {
-                    modelo = modelo.replace(`%{${textareaId}}`, result[textareaId]);
-                } else {
-                    alert("A chave '" + textareaId + "' não foi encontrada no armazenamento local");
-                }
-                resolve();  // Resolve a Promessa após a operação assíncrona ser concluída
-            });
-        });
+  var promessas = textareaIds.map(function (textareaId) {
+    return new Promise(function (resolve) {
+      chrome.storage.local.get(textareaId, function (result) {
+        if (textareaId in result) {
+          modelo = modelo.replace(`%{${textareaId}}`, result[textareaId]);
+        } else {
+          alert("A chave '" + textareaId + "' não foi encontrada no armazenamento local");
+        }
+        resolve();
+      });
     });
+  });
 
-    // Aguarda todas as Promessas serem resolvidas antes de exibir o modelo
-    Promise.all(promessas).then(function() {
-        salvarTextoComoArquivo(modelo);
-    });
+  Promise.all(promessas).then(function () {
+    navigator.clipboard.writeText(modelo).then(
+      function () {
+        alert("Texto copiado com sucesso!");
+      },
+      function (err) {
+        console.error("Falha ao copiar o texto: ", err);
+      }
+    );
+  });
 }
 
+function limparCampos() {
+  var textareas = document.getElementsByTagName('textarea');
+  for (var i = 0; i < textareas.length; i++) {
+    textareas[i].value = '';
 
-function salvarTextoComoArquivo(texto, nomeArquivo) {
-    // Cria um Blob com o texto
-    var blob = new Blob([texto], { type: 'text/plain' });
+    var data = {};
+    data[textareas[i].id] = '';
 
-    // Cria um URL temporário para o Blob
-    var url = window.URL.createObjectURL(blob);
-
-    // Cria um elemento de link
-    var link = document.createElement('a');
-
-    // Define o URL do link como o URL temporário
-    link.href = url;
-
-    // Define o atributo de download e o nome do arquivo
-    link.download = nomeArquivo || 'arquivo.txt';
-
-    // Adiciona o link ao corpo do documento
-    document.body.appendChild(link);
-
-    // Simula um clique no link para iniciar o download
-    link.click();
-
-    // Remove o link do corpo do documento
-    document.body.removeChild(link);
-
-    // Revoga o URL temporário
-    window.URL.revokeObjectURL(url);
+    chrome.storage.local.set(data).then(() => {
+      //
+    });
+  }
 }
 
